6eeae71a1811b904c4fabf783f723abafdbfdadb
ozozod <ozozojuegos@gmail.com>
2023-05-11 22:41:42 -0300

    target

--- src/test/java/com/portfolio/dj/DjApplicationTests.java
+++ src/test/java/com/portfolio/dj/DjApplicationTests.java
@@ -1,15 +1,13 @@
 package com.portfolio.dj;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DjApplicationTests {
 
 	@Test
-	public static void main(String[] args) {
-            SpringApplication.run(DjApplication.class, args);
+	void contextLoads() {
 	}
 
 }
