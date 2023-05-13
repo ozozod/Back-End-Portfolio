583771c2532bae7fb8c1c6f27a21c1ac496826fb
ozozod <ozozojuegos@gmail.com>
2023-05-13 01:08:55 -0300

    esta, esta es la que funciona de verdad pls

--- src/main/java/com/portfolio/jd/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/jd/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"https://back-endjd.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://front-end-jd.web.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
